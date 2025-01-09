1115e4b83f6ddfab91a93c89658d409c95a70676
Ralph-DnG <ralphmaronilla003@gmail.com>
2025-01-09 13:57:52 +0800

    update

--- app/api/accounts/[id]/route.ts
+++ app/api/accounts/[id]/route.ts
@@ -1,10 +1,10 @@
 import { NextResponse } from "next/server";
 
-import User from "@/database/user.model";
+import Account from "@/database/account.model";
 import handleError from "@/lib/handlers/error";
 import { NotFoundError, ValidationError } from "@/lib/http-errors";
 import dbConnect from "@/lib/mongoose";
-import { UserSchema } from "@/lib/validations";
+import { AccountSchema } from "@/lib/validations";
 import { APIErrorResponse } from "@/types/global";
 
 // get user by id
@@ -15,12 +15,12 @@ export async function GET(
 ) {
   const { id } = await params;
   if (!id) {
-    throw new NotFoundError("User not found");
+    throw new NotFoundError("Account not found");
   }
   try {
     await dbConnect();
-    const user = await User.findById(id);
-    if (!user) throw new NotFoundError("User not found");
+    const user = await Account.findById(id);
+    if (!user) throw new NotFoundError("Account not found");
     return NextResponse.json({ success: true, data: user }, { status: 200 });
   } catch (error) {
     return handleError(error, "api") as APIErrorResponse;
@@ -33,20 +33,26 @@ export async function PUT(
 ) {
   const { id } = await params;
   if (!id) {
-    throw new NotFoundError("User not found");
+    throw new NotFoundError("Account not found");
   }
   try {
     await dbConnect();
     const body = await request.json();
-    const validatedData = UserSchema.partial().parse(body);
+    const validatedData = AccountSchema.safeParse(body);
+    if (!validatedData.success) {
+      throw new ValidationError(validatedData.error.flatten().fieldErrors);
+    }
+    const updatedAccount = await Account.findByIdAndUpdate(
+      id,
+      validatedData.data,
+      {
+        new: true,
+      }
+    );
 
-    const updatedUser = await User.findByIdAndUpdate(id, validatedData, {
-      new: true,
-    });
-
-    if (!updatedUser) throw new NotFoundError("User not found");
+    if (!updatedAccount) throw new NotFoundError("Account not found");
     return NextResponse.json(
-      { success: true, data: updatedUser },
+      { success: true, data: updatedAccount },
       { status: 200 }
     );
   } catch (error) {
@@ -66,9 +72,9 @@ export async function DELETE(
   }
   try {
     await dbConnect();
-    const user = await User.findByIdAndDelete(id);
-    if (!user) throw new NotFoundError("User not found");
-    return NextResponse.json({ success: true, data: user }, { status: 204 });
+    const account = await Account.findByIdAndDelete(id);
+    if (!account) throw new NotFoundError("Account not found");
+    return NextResponse.json({ success: true, data: account }, { status: 204 });
   } catch (error) {
     return handleError(error, "api") as APIErrorResponse;
   }
